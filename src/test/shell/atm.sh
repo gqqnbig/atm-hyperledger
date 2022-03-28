9e82d23e99a39d8047507a4d35f9f91a71a20dd0
gqqnbig <gqqnb2005@gmail.com>
2022-03-28 14:51:04 +0800

    add naive testmanageCard

--- src/test/shell/atm.sh
+++ src/test/shell/atm.sh
@@ -86,5 +86,11 @@ testManageUser() {
 	fi
 }
 
+testmanageCard() {
+	pci -C mychannel -n atm --waitForEvent -c '{"function":"ManageBankCardCRUDServiceImpl:createBankCard","Args":["1","NORMAL","CREDIT","666","10"]}'
+	pci -C mychannel -n atm --waitForEvent -c '{"function":"ManageBankCardCRUDServiceImpl:queryBankCard","Args":["1"]}'
+	pci -C mychannel -n atm --waitForEvent -c '{"function":"ManageBankCardCRUDServiceImpl:modifyBankCard","Args":["1", "CANNEL", "DESPOSIT", "888","100"]}'
+	pci -C mychannel -n atm --waitForEvent -c '{"function":"ManageBankCardCRUDServiceImpl:deleteBankCard","Args":["1"]}'
+}
 
 source shunit2
